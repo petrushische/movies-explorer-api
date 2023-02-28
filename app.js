a82323b39babf91c36a2d01c5f626dc380bc4b9a
Пётр Румянцев <dogboxer64529000@gmail.com>
2023-02-28 18:15:01 +0300

    feat: Cors

--- app.js
+++ app.js
@@ -26,6 +26,25 @@ const movieRouter = require('./routes/movieRoutes');
 
 const app = express();
 
+const allowedCors = [
+  'http://localhost:3000',
+];
+// Cors
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  } if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
+
 app.use(requestLogger); //  логгер запросов
 
 app.post('/signin', express.json(), celebrate({
